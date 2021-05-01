1104a987208874554f01cae390d76bea2e9d021a
Jose <joey_manuel@outlook.com>
2021-05-01 19:29:21 -0400

    1. Initial Commit
    Implemented Jump Force
    Implemented background Sidecroller
    Implemetned Animation

---
+++ Prototype_3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30f;
+    private float leftBound = -15;
+
+    private PlayerController playerControllerScript;
+
+
+    private void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+
+    private void Update()
+    {
+        if(playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+
+
+} // main class
---
+++ Prototype_3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    #region Variables
+
+    private Vector3 startPos;
+
+    private float repeatWidth;
+
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+}
